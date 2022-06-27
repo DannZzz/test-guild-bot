3bf35665d4f8d690f5b01d94a49bd92264f047e9
Dann <46873266+Aeol-ian@users.noreply.github.com>
2022-06-27 15:52:18 +0400

    Add files via upload

---
+++ src/messageCommands/settings/reset-member.ts
@@ -0,0 +1,31 @@
+import { members } from "../../database/models/memberSchema";
+import { Embed } from "../../structures/Embed";
+import { MessageCommand } from "../../structures/MessageCommand";
+
+export default new MessageCommand ({
+    name: "reset-member",
+    description: "сбрасывать статистику участника",
+    cooldown: 2,
+    master: true,
+    category: 3,
+    examples: ["{prefix}reset-member @Dann#2523", "{prefix}reset-member all"],
+    async run ({msg, methods, args}) {
+        if (!args[0]) return methods.createError(msg, `Укажите участника, либо \`all\`, чтобы сбрасывать участников.`, 'reset-member').send();
+
+        const member = msg.mentions.members.first() || msg.guild.members.cache.get(args[0]);
+
+        if (!member && args[0].toLowerCase() !== "all") return methods.createError(msg, `Укажите участника, либо \`all\`, чтобы сбрасывать участников.`, 'reset-member').send();
+
+        if (member) {
+            await members.deleteOne({_id: member.id});
+            Embed(msg)
+                .setSuccess(`Успешно сброшена статистика ${member.user.username}.`)
+                .send();
+        } else {
+            await members.deleteMany();
+            Embed(msg)
+                .setSuccess(`Успешно сброшена статистика всех участников.`)
+                .send();
+        }
+    }
+})
